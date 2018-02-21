fab5432db2dc61622e9345020e5c4ab8c1998fe8
tfb34 <britothalia212@gmail.com>
2018-02-21 13:16:09 -0500

    Create basic game model

--- find-waldo/db/schema.rb
+++ find-waldo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219193128) do
+ActiveRecord::Schema.define(version: 20180221181045) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20180219193128) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["character_id"], name: "index_coordinates_on_character_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "numOfCharacters"
+    t.integer  "numOfFinds"
+    t.integer  "score"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "photographs", force: :cascade do |t|
